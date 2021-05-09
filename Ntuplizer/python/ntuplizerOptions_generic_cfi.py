1cb8dae50fd407045c954ae5c83ae60c1745b20b
Dipakroot <41667559+Dipakroot@users.noreply.github.com>
2021-05-09 19:50:54 +0530

    Update ntuplizerOptions_generic_cfi.py

--- Ntuplizer/python/ntuplizerOptions_generic_cfi.py
+++ Ntuplizer/python/ntuplizerOptions_generic_cfi.py
@@ -5,7 +5,7 @@ config = dict()
 #--------- general ----------#
 
 #--------- Set Just one to true ----------#
-config["RUNONMC"] = False
+config["RUNONMC"] = True
 #-----------------------------------------#
 
 #--------- For taus ----------#
@@ -16,11 +16,11 @@ config["VPROBCUT"] = 0.05
 config["DNNCUT"] = 0.1443
 
 
-#config["USEJSON"] = not (config["RUNONMC"])
-config["USEJSON"] = False
+config["USEJSON"] =  (config["RUNONMC"])
+config["USEJSON"] = True
 #config["JSONFILE"] = "JSON/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt" #data 2017
 #config["JSONFILE"] = "JSON/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt" # data 2016
-config["JSONFILE"] = "JSON/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt" # data 2018
+#config["JSONFILE"] = "JSON/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt" # data 2018
 config["USENOHF"] = False
 
 
@@ -33,9 +33,9 @@ config["DOMISSINGET"] = True
 
 config["DOJPSIMU"] = False
 config["DOJPSITAU"] = False
-config["DOBSTAUTAU"] = False
+config["DOBSTAUTAU"] = True
 config["DOBSTAUTAUFH"] = False
-config["DOBSDSTARTAUNU"] = True
+config["DOBSDSTARTAUNU"] = False
 config["ISTRUTH"] = False
 
 config["DOGENHIST"] = (True and config["RUNONMC"]);
@@ -44,9 +44,10 @@ if config["DOJPSITAU"]:
     config["DNNFILE"] = "data/DNN/BcJPsi/DUMMY"
 
 if config["DOBSTAUTAU"] or config["DOBSTAUTAUFH"] or config["DOBSDSTARTAUNU"]:
-    config["DNNFILE"] = "data/DNN/BsTauTau/DUMMY"    
+    #config["DNNFILE"] = "data/DNN/BsTauTau/DUMMY" 
+    config["DNNFILE"] = "mc/DNN/BsTauTau/DUMMY" 
 
 #--------- JEC ----------#
 
-config["CORRMETONTHEFLY"] = False  # at the moment JEC available just for MC Fall17#
+config["CORRMETONTHEFLY"] = True  # at the moment JEC available just for MC Fall17
 
